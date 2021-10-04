a62df670f36191e6747ab02b9e7f2b47f3bb3605
Jim <jim.haz34@gmail.com>
2021-10-04 12:41:37 -0400

    push to Netlify test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ah, push it💪</h1>
+      <p>local to Github to Netlify</p>
+      <a href="https://www.youtube.com/watch?v=dPfTiX2i4Vk"><button>🧂Salt and Pepa's here - Push it good</button></a>
     </div>
   );
 }
